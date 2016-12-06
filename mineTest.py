736abcc0510f98f8e43b891c1572ae086e66b0d2
Jacques Cadet <Python3-77@users.noreply.github.com>
2016-12-05 20:00:28 -0500

    Update mineTest.py

--- mineTest.py
+++ mineTest.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-from unittest import TestCase as TC, main as MM
+from unittest import TestCase as TC, main
 from autogrids import MineField as MF, AutoGrid
 
 class Grids(TC):
 
-            Test_input ={
+            Test_input = {  #test input for MineField.
                 
                         1:['*','*','*'],
                         2:['*','.','*'],
                         3:['*','*','*']
             }
 
-            Test_grids ={
+            Test_grids = {  #grids the output is to be compared to.
         
                         1 : {1:['*','*','*'],2:['*','0','*'],3:['*','*','*']},#prep(grid)-- '.'  to '0'
                         2 : {1:['*','*','*'],2:['*','2','*'],3:['*','*','*']},#scan(gridX)
@@ -23,14 +23,15 @@ class Grids(TC):
             
            
                                         
-            m=MF(3,3,3)
-            n = AutoGrid(3,3,3)#there is no way for me to create an assertion case, the output is random
+            m = MF(3,3,3)
+            n = AutoGrid(3,3,3) #there is no way for me to create an definite assertion case, the output is random
             
-            Tg=Test_grids
+            Tg = Test_grids
             Ti = Test_input
-            m.d=Ti
+            m.d = Ti
             
             def test_Auto_grid(self):
+                    '''Tests the method that creates random grids'''
                  
                  self.n.make_grid()
                  self.n.add_random()
@@ -38,30 +39,34 @@ class Grids(TC):
                  self.n.show(self.n.grid)#passes if it runs through methods without error
                         
             def test_Prep(self):
-                    '''Prepares grid for integers'''
+                    '''Tests the method that prepares grid for integers'''
 
                     self.m.prep(self.m.d)
                     self.assertEqual(self.m.gridX,self.Tg[1])
                     
             def test_Scanner(self):
+                    '''Tests the methods that proccess the grid. '''
                 
                 self.m.scan()#4 functions called, one repeated  
-                #Grids X Y Z Processed, Check results
+                #Grids X Y Z Processed
                 
              
             def test_XScan_grid(self):
+                    '''Confirms gridX matches the corresponding test grid.'''
                                             
-                                            ''' X-Scan'''
+                                            ''' X-Scan '''
                                                                                                 
                                             self.assertEqual(self.m.gridX,self.Tg[2])
                                             
             def test_YScan_grid(self):
+                    '''Confirms gridY matches the corresponding test grid.'''
                       
                                             ''' Y-Scan'''
                                                         
                                             self.assertEqual(self.m.gridY,self.Tg[3])
                                             
             def test_ZScan_grid(self):
+                    '''Confirms gridZ matches the corresponding test grid.'''
                       
                                             ''' Z-Scan'''
                                             
@@ -69,7 +74,7 @@ class Grids(TC):
                             
 if __name__=='__main__':
 
-                        MM()
+                        main()
                     
                                         
 
